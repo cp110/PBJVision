a49677402b911cf50b6d58bb258bf90f72691810
c p <1107223894@qq.com>
2018-10-25 00:53:50 +0800

    carthage

---
+++ PBJVisionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PBJVisionDemo
+//
+//  Created by chenp on 2018/10/25.
+//  Copyright © 2018 chenp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PBJVisionOC/PBJVisionOC.h
@@ -0,0 +1,22 @@
+//
+//  PBJVisionOC.h
+//  PBJVisionOC
+//
+//  Created by chenp on 2018/10/25.
+//  Copyright © 2018 chenp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PBJVisionOC.
+FOUNDATION_EXPORT double PBJVisionOCVersionNumber;
+
+//! Project version string for PBJVisionOC.
+FOUNDATION_EXPORT const unsigned char PBJVisionOCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PBJVisionOC/PublicHeader.h>
+
+#import <PBJVisionOC/PBJVision.h>
+#import <PBJVisionOC/PBJGLProgram.h>
+#import <PBJVisionOC/PBJMediaWriter.h>
+#import <PBJVisionOC/PBJVisionUtilities.h>
