5478705fd65397ccc37ce081e7584d78bd438c45
c p <1107223894@qq.com>
2018-10-25 00:59:22 +0800

    公开.h

--- Source/PBJVisionUtilities.h
+++ Source/PBJVisionUtilities.h
@@ -23,7 +23,7 @@
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
 
 @interface PBJVisionUtilities : NSObject
